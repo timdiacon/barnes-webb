d0fd0d98efa1a0aba6aa87bc0e8b9cd20007f88f
Nukeourlimo <nukeourlimo@gmail.com>
2015-08-02 11:19:14 +0100

    making sure repo is right, strange issues with git

---
+++ dev/js/shopData.js
@@ -0,0 +1,19 @@
+var shopData = [
+	["Adoption Thyme","£25","img/shop/B&W_shop_images_thyme.jpg","TRUE","TRUE"],
+	["Adoption Poppy","£50","img/shop/B&W_shop_images_poppy.jpg","TRUE","TRUE"],
+	["Adoption Sunflower","£85","img/shop/B&W_shop_images_sunflower.jpg","TRUE","TRUE"],
+	["Adoption Cherry","£250","img/shop/B&W_shop_images_cherry.jpg","TRUE","TRUE"],
+	["Adoption Willow","£500","img/shop/B&W_shop_images_willow.jpg","TRUE","TRUE"],
+	["Save the Bees A1 Poster","£50","img/shop/B&W_shop_images_poster.jpg","TRUE","TRUE"],
+	["Save the Bees Tote Bag (Black)","£10","img/shop/B&W_shop_images_tote_black.jpg","TRUE","TRUE"],
+	["Save the Bees Tote Bag (White)","£10","img/shop/B&W_shop_images_tote_white.jpg","TRUE","TRUE"],
+	["Postcode Honey - E5","£8.50","img/shop/B&W_shop_images_E5_227g.jpg","FALSE","TRUE"],
+	["Postcode Honey - EC2","£8.50","img/shop/B&W_shop_images_EC2_227g.jpg","FALSE","TRUE"],
+	["Postcode Honey - E8","£8.50","img/shop/B&W_shop_images_E8_227g.jpg","FALSE","TRUE"],
+	["Postcode Honey - SE14","£8.50","img/shop/B&W_shop_images_SE14_227g.jpg","FALSE","TRUE"],
+	["Postcode Honey - N1","£8.50","img/shop/B&W_shop_images_N1_227g.jpg","FALSE","TRUE"],
+	["Postcode Honey - N16","£8.50","img/shop/B&W_shop_images_N16_227g.jpg","FALSE","TRUE"],
+	["Postcode Honey - SW15","£8.50","img/shop/B&W_shop_images_SW15_227g.jpg","FALSE","TRUE"],
+	["Postcode Honeycomb - EC2","£10.00","img/shop/B&W_shop_images_EC2_comb.jpg","FALSE","TRUE"],
+	["Postcode Honeycomb - SE14","£10.00","img/shop/B&W_shop_images_SE14_comb.jpg","FALSE","TRUE"]
+];
